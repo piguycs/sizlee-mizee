492e57bb568781baf419548302c8653f421531b1
RocKing1001 <44892832+RocKing1001@users.noreply.github.com>
2019-11-03 11:57:26 +0100

    Added all the bot files
    Added all the bot files

---
+++ SizelyMizee/index.js
@@ -0,0 +1,49 @@
+const Discord = require('discord.js')
+const client = new Discord.Client()
+require('dotenv').config()
+
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`)
+})
+
+client.on('guildMemberAdd' , member => {
+  member.send(`Welcome on the server! Please be aware that we won’t tolerate troll, spam or harassment. Have fun `)
+})
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!')
+  }
+})
+
+client.on('message', msg => {
+  if (msg.content === '!hi') {
+    msg.send('I am online')
+  }
+})
+
+client.on('message', msg => {
+  if (msg.content === '!secret') {
+    msg.reply('Do you know one special thing....? I am a BOT!!! :grin:  :grin:  :grin: ')
+  }
+})
+
+client.on('message', msg => {
+  if (msg.content === '!friend') {
+    msg.reply('Friends forever!')
+  }
+})
+
+client.on('message', msg => {
+  if (msg.content === '!creeper') {
+    msg.reply('Aw Man')
+  }
+})
+
+client.on('message', msg => {
+  if (msg.content === '!so we back in the mine') {
+    msg.reply('Got our picaxe swinging from side to side')
+  }
+})
+
+client.login(process.env.BOT_TOKEN)
