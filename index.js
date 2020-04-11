9deeeaea1f9c59861c76ea4bd5a955b088d43c41
RocKing1001 <kdandekar@britams.nl>
2020-04-11 09:02:14 +0000

    Not much XD. the ping command

--- index.js
+++ index.js
@@ -3,47 +3,48 @@ const client = new Discord.Client()
 require('dotenv').config()
 
 client.on('ready', () => {
+  client.user.setGame('idk what I am playing lol')
+})
+
+client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
 })
 
-client.on('guildMemberAdd' , member => {
-  member.send(`Welcome on the server! Please be aware that we won’t tolerate troll, spam or harassment. Have fun `)
-})
-
 client.on('message', msg => {
-  if (msg.content === 'ping') {
-    msg.reply('Pong!')
+  if (msg.content === '!ping') {
+    msg.channel.send('Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms')
   }
 })
 
 client.on('message', msg => {
   if (msg.content === '!hi') {
-    msg.reply('I am online')
+    msg.channel.send('I am online')
   }
 })
 
 client.on('message', msg => {
   if (msg.content === '!secret') {
-    msg.reply('Do you know one special thing....? I am a BOT!!! :grin:  :grin:  :grin: ')
+    msg.channel.send('Do you know one special thing....? I am a BOT!!! :grin:  :grin:  :grin: ')
   }
 })
 
 client.on('message', msg => {
   if (msg.content === '!friend') {
-    msg.reply('Friends forever!')
+    msg.channel.send('Friends forever!')
   }
 })
 
 client.on('message', msg => {
   if (msg.content === '!creeper') {
-    msg.reply('Aw Man')
+    msg.channel.send('Aw man!!')
   }
 })
 
 client.on('message', msg => {
   if (msg.content === '!so we back in the mine') {
-    msg.reply('Got our picaxe swinging from side to side')
+    msg.channel.send('Got our pickaxe swinging from side to side')
   }
 })
+
 
 client.login(process.env.BOT_TOKEN)
