d030feee14791b65e552021d97f758e18899b913
RocKing1001 <kdandekar@britams.nl>
2020-04-11 09:09:40 +0000

    Advanced ping

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ client.on('guildMemberAdd' , member => {
 
 client.on('message', msg => {
   if (msg.content === 'ping') {
-    msg.reply('Pong!')
+    msg.reply(`Pong! Latency is ${m.createdTimestamp - message.createdTimestamp}ms. API Latency is ${Math.round(client.ping)}ms`)
   }
 })
 
